15fcf8879ab93fc51c6797682c6d0087dd121bf4
222hojun <sciboviator@gmail.com>
2024-01-31 14:30:50 +0900

    [test] 프로그램 test

---
+++ src/main/java/com/ohgiraffers/test/TestApplication.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.test;
+
+public class TestApplication {
+
+    // 여기는 주석입니다. 한줄 주석
+    /*여기는
+    * 여러줄
+    * 주석입니다*/
+    //수업목표 todo에 저장된 색깔
+    //목차 파란색
+    //필기 빨간색
+
+
+
+    /*
+    *  ctrl - space : 자동완성 기능
+    *  ctrl - d : 복제
+    * */
+
+
+    //복사예시
+    //복사예시
+    //복사예시
+    //복사예시
+    //복사예시
+
+    public static void main(String[] args) {
+        System.out.println("저는 개발자 입니다." +
+
+                "하나더 하나더하나더 하나더 하나더 하난더");
+        System.out.println("dkdkdkdk");
+
+    }
+
+    //sout : systemout.printLn 단축기
+    //shift - F10로그 : 프로그램 실행 단축키
+
+
+}
+
