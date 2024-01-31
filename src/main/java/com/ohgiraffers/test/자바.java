49a6bc9900655298d53ca1903be282f3e70624ef
222hojun <sciboviator@gmail.com>
2024-01-31 17:46:49 +0900

    [test] 프로그램 test-2

---
+++ src/main/java/com/ohgiraffers/test/자바.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.test;
+
+public class 자바 {
+    public static void main(String[] args) {
+        System.out.println("nenenenenenenenenenenenenenenenenenenennene");
+    }
+
+
+
+}
